698a73a9d023036edf1e013c4a89757179c57c4a
LeandrodeLimaC <leandrolima7472@gmail.com>
2022-12-22 20:09:49 -0300

    feat: add user and get users todos

--- src/index.js
+++ src/index.js
@@ -1,25 +1,63 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  if(!username) {
+    return response.status(400).json({error: 'Username is required'})
+  }
+
+  const userFound = users.find((user) => user.username === username )
+
+  if(!userFound) {
+    return response.status(404).json({error: 'Username not found'})
+  }
+
+  request.user = userFound
+  next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {body} = request
+  
+  if(!body.name || !body.username) {
+    return response.status(400).json({error: 'Name and Username are required'})
+  }
+
+  const userFound = users.find((user) => user.username === body.username )
+
+  if(userFound) {
+    return response.status(400).json({error: 'Username already in use'})
+  }
+
+  const user = {
+    id: uuidv4(),
+    name: body.name, 
+    username: body.username, 
+    todos: []
+  }
+
+  users.push(user)
+
+  return response.status(201).json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  console.log('ENTROU')
+  const {user} = request
+
+  console.log('user todos', user.todos)
+
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
